16b953cf348596b5bb43f2ded2275fb3dce597d9
Edwin Kariuki <kariukimugo510@icloud.com>
2026-08-27 21:40:09 +0300

    homepage

--- GDLite/src/App.jsx
+++ GDLite/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import HeroImg from './assets/images/101.jpg'
 
 function App() {
 
   return (
     <>
-      <p>GDLite</p>
-
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="relative w-full h-full">
+          <img src={HeroImg} className="w-full h-full object-contain" />
+          <p className="absolute top-1/4 left-1/5 -translate-x-1/2 -translate-y-1/2 text-4xl md:text-6xl font-extrabold whitespace-nowrap">
+            <span className="text-[#EFA24C]">GD</span>
+            <span className="text-[#2547A8]">LITE</span>
+            <span className="text-black">AFRICA</span>
+          </p>
+        </div>
+      </div>
     </>
   )
 }
