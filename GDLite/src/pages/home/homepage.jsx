72e4ae040b8aa883393668fd20caba12f24faa05
Edwin Kariuki <kariukimugo510@icloud.com>
2026-09-10 23:59:15 +0300

    update homepage

--- GDLite/src/pages/home/homepage.jsx
+++ GDLite/src/pages/home/homepage.jsx
@@ -1,6 +1,6 @@
 import HeroImg from '../../assets/images/105.jpg'
 import WorkersSolarImg from '../../assets/images/workers_solar.jpg'
-import CctvImg from '../../assets/images/cctv_image.jpg'
+import CctvImg from '../../assets/images/cctv.jpg'
 import SeverImg from '../../assets/images/server_room.jpg'
 import StackingCard from '../../components/stacking_card'
 
